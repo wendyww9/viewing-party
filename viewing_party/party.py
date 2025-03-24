bdb206ffc28d334e485f875d8d9f713c2197204f
Lina Martinez <lmcastro3501@gmail.com>
2025-03-24 11:53:49 -0700

    implements watch_movie function. Wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,6 +18,20 @@ def add_to_watchlist(user_data,movie):
     user_data["watchlist"].append(movie)
     return user_data
 
+def watch_movie(user_data,title):
+#     - if title in watchlist
+#     - remove movie from watchlist
+#     - add movie to watched
+#     - return user_data
+# - else:
+#     - return user_data
+    wacthlist = user_data["watchlist"]
+
+    for movie in wacthlist:
+        if title == movie["title"]:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
