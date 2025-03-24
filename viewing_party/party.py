a79398cf193126c7f3813772f0c04f5d90a3ee86
Lina Martinez <lmcastro3501@gmail.com>
2025-03-24 11:28:14 -0700

    implements create_movie function. Wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,22 @@
 # ------------- WAVE 1 --------------------
-
+# - create_movie(`title`, `genre`, `rating`)
+# - initi
+# - if truth
+#     - movie_dict={”`titl`e": title, ”`genre`": genre, ”`rating`":rating}
+# - else
+#     - return None
+# - return movie_dict
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating: 
+        movie_dict = {
+            "title": title,
+            "genre": genre,
+            "rating": rating
+        }
+        return movie_dict
+    else: 
+        return None
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
