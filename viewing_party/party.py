4577543b65fe8310f76a40e1f36f7a4a6db4b071
Lina Martinez <lmcastro3501@gmail.com>
2025-03-26 14:41:32 -0700

    implements get_available_recs function in Wave 4

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -89,12 +89,19 @@ def get_friends_unique_watched(user_data):
     
     return friends_unique_movies
 
-
-        
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+def get_available_recs(user_data):
 
+    friends_unique_watched = get_friends_unique_watched(user_data)
+    user_subscriptions = user_data.get("subscriptions", [])
+
+    recommendations = []
+
+    recommendations = [movie for movie in friends_unique_watched if movie["host"] in user_subscriptions]
+
+    return recommendations
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
