849f3ffe93f6a2ad3300d742e1d7fb3fb8a13f5e
wendyww9 <wendyskystar@gmail.com>
2025-03-26 14:20:03 -0700

    completes functions for wave 3

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -60,6 +60,35 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    
+    user_watched = user_data.get("watched", [])
+    friends_watched = []
+    user_unique_movies = []
+
+    for friend in user_data.get("friends", []):
+        friends_watched.extend(friend.get("watched", []))
+
+    for movie in user_watched:
+        if movie not in friends_watched and movie not in user_unique_movies:
+            user_unique_movies.append(movie)
+    
+    return user_unique_movies
+
+def get_friends_unique_watched(user_data):
+    user_watched = user_data.get("watched", [])
+    friends_watched = []
+    friends_unique_movies= []
+
+    for friend in user_data.get("friends", []):
+        friends_watched.extend(friend.get("watched", []))
+        
+    for movie in friends_watched:
+        if movie not in friends_unique_movies and movie not in user_watched:
+            friends_unique_movies.append(movie)
+    
+    return friends_unique_movies
+
 
         
 # -----------------------------------------
