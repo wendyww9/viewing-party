5ac8c2becd8820d63cd36e54d0c405970037bb54
Lina Martinez <lmcastro3501@gmail.com>
2025-03-27 14:17:20 -0700

    adds doc strings for functions.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,24 +18,34 @@ def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
 
-def watch_movie(user_data,title):
+def watch_movie(user_data, title):
     wacthlist = user_data["watchlist"]
 
     for movie in wacthlist:
         if title == movie["title"]:
             user_data["watchlist"].remove(movie)
             user_data["watched"].append(movie)
+
     return user_data
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 def get_watched_avg_rating(user_data):
-    
+    """
+    Calculates the average rating of watched movies.
+
+    Returns:
+    float: The average rating of watched movies, or 0.0 if none are watched.
+    """
+
     avg_rating = 0.0
     count = 0
     sum = 0
+
     if not user_data["watched"]:
         return avg_rating
+    
     for movie in user_data["watched"]:
         sum += movie["rating"]
         count += 1
@@ -44,24 +54,39 @@ def get_watched_avg_rating(user_data):
     return avg_rating
 
 def get_most_watched_genre(user_data):
+    """
+    Determines the most watched genre among watched movies.
+
+    Returns:
+    str: The most watched genre, or None if no movies have been watched.
+    """
+
     movie_genre = {}
     count = 0
     genre = ""
+
     if not user_data["watched"]:
         return None
+    
     for movie in user_data["watched"]:
         movie_genre[movie["genre"]] = movie_genre.get(movie["genre"], 0) + 1
         if count < movie_genre[movie["genre"]]:
             count = movie_genre[movie["genre"]]
             genre = movie["genre"]
+
     return genre
 
-    
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 def get_unique_watched(user_data):
-    
+    """
+    Finds movies that the user has watched but their friends haven't.
+
+    Returns:
+    list: A list of movies watched only by the user.
+    """
+
     user_watched = user_data.get("watched", [])
     friends_watched = []
     user_unique_movies = []
@@ -76,6 +101,13 @@ def get_unique_watched(user_data):
     return user_unique_movies
 
 def get_friends_unique_watched(user_data):
+    """
+    Finds movies that friends have watched but the user hasn't.
+
+    Returns:
+    list: A list of movies watched only by friends.
+    """
+
     user_watched = user_data.get("watched", [])
     friends_watched = []
     friends_unique_movies= []
@@ -93,11 +125,16 @@ def get_friends_unique_watched(user_data):
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 def get_available_recs(user_data):
+    """
+    Recommends movies that friends have watched but the user hasn't, 
+    and are available on the user's subscribed services.
+
+    Returns:
+    list: A list of recommended movies.
+    """
 
     friends_unique_watched = get_friends_unique_watched(user_data)
     user_subscriptions = user_data.get("subscriptions", [])
-
-    recommendations = []
 
     recommendations = [movie for movie in friends_unique_watched if movie["host"] in user_subscriptions]
 
@@ -107,6 +144,13 @@ def get_available_recs(user_data):
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 def get_new_rec_by_genre(user_data):
+    """
+    Recommends movies from friends' unique watched list that match the user's most watched genre.
+
+    Returns:
+    list: A list of recommended movies in the most watched genre.
+    """
+        
     friends_unique_watched = get_friends_unique_watched(user_data)
     common_genre = get_most_watched_genre(user_data)
 
@@ -115,6 +159,13 @@ def get_new_rec_by_genre(user_data):
     return recommendations
 
 def get_rec_from_favorites(user_data):
+    """
+    Recommends movies that are in the user's favorites list and are unique to the user.
+
+    Returns:
+    list: A list of recommended movies from favorites.
+    """
+
     favorites = user_data.get("favorites", [])
     unique_watched = get_unique_watched(user_data)
 
